1930d248d180c19bc8763fc453c6e1a43b7a7ddb
gr <grim322@bk.ru>
2022-08-24 13:58:06 +0300

    hw_7

--- cool_service/frontend/src/components/Menu.jsx
+++ cool_service/frontend/src/components/Menu.jsx
@@ -1,23 +1,29 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 
-const Menu = () => {
+const Menu = ({logout, username, isLogin}) => {
     return (
         <div>
             <nav className="navbar navbar-light bg-light">
-                <div className="container-fluid">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
-                        <li className="nav-item">
-                            <Link to="/users">Пользователи</Link>
+                <div className="container-fluid ">
+                    <ul className="navbar-nav flex-row">
+                        <li className="nav-item ms-5">
+                            <Link className="link-secondary" to="/users">Пользователи</Link>
                         </li>
-                        <li className="nav-item">
-                            <Link to="/projects">Проекты</Link>
+                        <li className="nav-item ms-5">
+                            <Link className="link-secondary" to="/projects">Проекты</Link>
                         </li>
-                        <li className="nav-item">
-                            <Link to="/todos">Todos</Link>
+                        <li className="nav-item ms-5">
+                            <Link className="link-secondary " to="/todos">Todos</Link>
+                        </li>
+                        <li className="nav-item ms-5">
+                            { isLogin
+                                ? <p>{username} <a className="link-secondary" style={{cursor: 'pointer'}} onClick={logout}>Logout</a></p>
+                                : <Link className="link-secondary " to="/login">Sign in</Link>
+                            }
+
                         </li>
                     </ul>
-
                 </div>
             </nav>
         </div>
